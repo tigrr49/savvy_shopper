cb1639d7baec0b7553db6d518ef2d8b93b9e157d
Paul K Nguyen <paul.nguyen@kellogg.northwestern.edu>
2017-06-03 17:58:28 -0500

    DB Issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,116 @@
 
 User.destroy_all
 
-u1 = User.new
-u1.email = "paul.nguye@kellogg.northwestern.edu"
-u1.password = "pd2017"
-u1.password_confirmation = "12341234"
-u1.first_name = "Paul"
-u1.last_name = "Nguyen"
-u1.save
+a = User.new
+a.email = "john.smith@sample.com"
+a.password = "12341234"
+a.password_confirmation = "12341234"
+a.first_name = "John"
+a.last_name = "Smith"
+a.save
+
+b = User.new
+b.email = "mary.jones@sample.com"
+b.password = "12341234"
+b.password_confirmation = "12341234"
+b.first_name = "Mary"
+b.last_name = "Jones"
+b.save
+
+c = User.new
+c.email = "tom.brown@sample.com"
+c.password = "12341234"
+c.password_confirmation = "12341234"
+c.first_name = "Tom"
+c.last_name = "Brown"
+c.save
 
 puts "#{User.count} users in the database"
+
+
+Store.destroy_all
+
+a = Store.new
+a.name = "Jewel-Osco"
+a.save
+
+b = Store.new
+b.name = "Food4Less"
+b.save
+
+c = Store.new
+c.name = "Valli Produce"
+c.save
+
+puts "#{Store.count} stores in the database"
+
+
+Category.destroy_all
+
+a = Category.new
+a.name = "Frozen"
+a.save
+
+b = Category.new
+b.name = "Canned"
+b.save
+
+c = Category.new
+c.name = "Produce"
+c.save
+
+puts "#{Category.count} categories in the database"
+
+
+Item.destroy_all
+
+a = Item.new
+a.category_id = 1
+a.name = "Pizza"
+a.brand = "DiGiorno"
+a.size = 31
+a.save
+
+b = Item.new
+b.name = "Canned"
+b.save
+
+c = Item.new
+c.name = "Produce"
+c.save
+
+puts "#{Item.count} items in the database"
+
+
+Category.destroy_all
+
+a = Category.new
+a.name = "Frozen"
+a.save
+
+b = Category.new
+b.name = "Canned"
+b.save
+
+c = Category.new
+c.name = "Produce"
+c.save
+
+puts "#{Category.count} categories in the database"
+
+
+Category.destroy_all
+
+a = Category.new
+a.name = "Frozen"
+a.save
+
+b = Category.new
+b.name = "Canned"
+b.save
+
+c = Category.new
+c.name = "Produce"
+c.save
+
+puts "#{Category.count} categories in the database"
