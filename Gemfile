e1511490254064da5c671e6dc2c0afb5bb1d80da
Paul K Nguyen <paul.nguyen@kellogg.northwestern.edu>
2017-06-03 16:55:11 -0500

    With Default Public Homepage

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ group :development do
 end
 
 
+gem "binding_of_caller"
+
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.3'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+u1 = User.new
+u1.email = "paul.nguye@kellogg.northwestern.edu"
+u1.password = "pd2017"
+u1.password_confirmation = "12341234"
+u1.first_name = "Paul"
+u1.last_name = "Nguyen"
+u1.save
+
+puts "#{User.count} users in the database"
